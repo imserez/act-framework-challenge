08363a1c2d43ddb821d5d3f0bae83bed03dc0f1c
Sergi <sergijp22@gmail.com>
2026-05-11 17:48:59 +0200

    add: non-blocking read, using select

--- main.c
+++ main.c
@@ -3,6 +3,12 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+#include <sys/select.h>
+#include <string.h>
+#include <errno.h>
+
+#define TIMEOUT_SEC 2
+
 int serial_open(char *port, int baud)
 {
     int fd;
@@ -64,12 +70,57 @@ int main (int argc, char *argv[])
     char msg[] = "hey";
     write(fd, msg, sizeof(msg));
 
-    char response[100];
 
-    read(fd, response, 100);
+    // addind non-blocking using select
 
-    printf("the response was: [%s]\n", response);
+    fd_set read_fds;
+    struct timeval timeout;
+    FD_ZERO(&read_fds);
+    FD_SET(fd, &read_fds);
+
+    timeout.tv_sec = TIMEOUT_SEC;
+    timeout.tv_usec = 0;
+
+    printf("Waiting %d seconds", TIMEOUT_SEC);
+
+    int rv = select(fd + 1, &read_fds, NULL, NULL, &timeout);
+
+    if (rv == -1)
+    {
+        perror("select error");
+    } else if (rv == 0)
+    {
+        printf("timeout reached, no data received\n");
+    }
+    else
+    {
+        char res_buf[256];
+        memset(&res_buf, '\0', sizeof(res_buf));
+
+        int bread = read(fd, res_buf, sizeof(res_buf) - 1);
+        if (bread < 0)
+        {
+            fprintf(stderr, "erorr %d reading: [%s]\n", errno, strerror(errno));
+        }
+        else if (bread > 0)
+        {
+            printf("\n--- Data Received ---\n");
+            printf("Bytes read: %d\n", bread);
+            printf("Payload: %s\n", res_buf);
+            printf("---------------------\n");
+        }
+    }
+
+
+
+    // char response[100];
+
+    // read(fd, response, 100);
+
+    // printf("the response was: [%s]\n", response);
 
     printf("exiting...");
+
+    close(fd);
     return 0;
 }
